fe64b80775be6aa9142fe97e748e79cc46d202be
OCMT.EDU.OM <rawahsaif@gmail.com>
2025-12-11 18:14:34 +0400

    Create firebase-init.js

---
+++ firebase-init.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACuoVWxvF2jvHpfyjdtVXL1h1VZFr0YsQ",
+  authDomain: "smartmeal-7f276.firebaseapp.com",
+  databaseURL: "https://smartmeal-7f276-default-rtdb.firebaseio.com",
+  projectId: "smartmeal-7f276",
+  storageBucket: "smartmeal-7f276.firebasestorage.app",
+  messagingSenderId: "291729989038",
+  appId: "1:291729989038:web:cc99a3818578b488c3bc3f",
+  measurementId: "G-W2MNS8BQGQ"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
